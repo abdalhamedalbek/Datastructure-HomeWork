b014fd412640e41847deb2350bc1393650a226e2
abdalhamedalbek <101070815+abdalhamedalbek@users.noreply.github.com>
2025-12-26 19:53:28 +0300

    Circular Linked list Deleting, Searching, Traversel Code.cpp
    Circular Linked list Deleting, Searching, Traversel Code

--- Home work(6).cpp
+++ Home work(6).cpp
@@ -1,61 +1,191 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX 100
+struct node {
+	int data;
+	struct node *next;
+}*head=NULL;
 
-int graph[MAX][MAX];   // adjacency matrix
-int visited[MAX];
-int n; // number of nodes
+struct node* insert (struct node *head);
+void print (struct node *head);
+void sum (struct node *head);
+void avg (struct node *head);
+struct node* del (struct node *head);
+void num_of_nodes (struct node *head);
+struct node* destroy (struct node *head);
 
-void dfs(int node) {
-    printf("%d ", node);
-    visited[node] = 1;
-
-    for (int i = 0; i < n; i++) {
-        if (graph[node][i] == 1 && !visited[i]) {
-            dfs(i);
-        }
-    }
+int main () {
+	while (1) {
+		int x;
+		printf ("1.Insert a number\n2.Print the list\n3.Calculate the sum\n4.Calculate the average\n5.Delete an element\n6.Number of nodes\n7.Destroy the list\n8.Exit\n");
+		scanf ("%d",&x);
+		switch (x) {
+			case 1:head=insert(head); break;
+			case 2:print(head); break;
+			case 3:sum(head); break;
+			case 4:avg(head); break;
+			case 5:head=del(head); break;
+			case 6:num_of_nodes(head); break;
+			case 7:head=destroy(head); break;
+			case 8:exit(0); break;
+		}
+		printf ("_______________________________________________________\n");
+	}
 }
 
-int main() {
-    int edges, u, v;
-
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-
-    printf("Enter number of edges: ");
-    scanf("%d", &edges);
-
-    // Initialize graph
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            graph[i][j] = 0;
-
-    // Read edges
-    printf("Enter each edge (u v):\n");
-    for (int i = 0; i < edges; i++) {
-        scanf("%d %d", &u, &v);
-        graph[u][v] = 1;
-        graph[v][u] = 1;   // undirected graph
-    }
-
-    // Initialize visited array
-    for (int i = 0; i < n; i++)
-        visited[i] = 0;
-
-    int component = 1;
-
-    printf("\nConnected Components (Clusters):\n");
-
-    for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            printf("Cluster %d: ", component);
-            dfs(i);
-            printf("\n");
-            component++;
-        }
-    }
-
-    return 0;
+struct node* insert (struct node *head) {
+	struct node *newnode=(struct node *)malloc(sizeof(struct node));
+	printf ("Enter a number to insert it:\n");
+	scanf ("%d",&newnode->data);
+	if (head==NULL) {
+		head=newnode;
+		head->next=head;
+	}
+	else {
+		struct node *last,*q,*p=head;
+		while (p->next!=head) p=p->next;
+		last=p;
+		p=head;
+		while (p->next!=head && p->data<newnode->data) {
+			q=p;
+			p=p->next;
+		}
+		if (p==head && p->data>newnode->data) {
+			head=newnode;
+			newnode->next=p;
+			last->next=head;
+		}
+		else if (p->next!=head) {
+			q->next=newnode;
+			newnode->next=p;
+		}
+		else {
+			if (p->next==head && p->data>newnode->data) {
+				q->next=newnode;
+				newnode->next=p;
+			}
+			else if (p->next==head && p->data<newnode->data) {
+				p->next=newnode;
+				newnode->next=head;
+			}
+		}
+	}
+	printf ("**DONE**\n");
+	return head;
 }
 
+void print (struct node *head) {
+	if (head==NULL) printf ("The list is empty\n");
+	else {
+		struct node *p=head;
+		printf ("The list is:\n");
+		do {
+			printf ("%d\n",p->data);
+			p=p->next;
+		}while (p!=head);
+		printf ("**DONE**\n");
+	}
+}
+
+void sum (struct node *head) {
+	if (head==NULL) printf ("The list is empty\n");
+	else {
+		struct node *p=head;
+		int sum=0;
+		do {
+			sum+=p->data;
+			p=p->next;
+		}while(p!=head);
+		printf ("The sum is:%d\n",sum);
+		printf ("**DONE**\n");
+	}
+}
+
+void avg (struct node *head) {
+	if (head==NULL) printf ("The list is empty\n");
+	else {
+		struct node *p=head;
+		int avg,sum=0,c=0;
+		do {
+			c++;
+			sum+=p->data;
+			p=p->next;
+		}while(p!=head);
+		avg=sum/c;
+		printf ("The average is:%d\n",avg);
+		printf ("**DONE**\n");
+	}
+}
+
+struct node* del (struct node *head) {
+	if (head==NULL) printf ("The list is empty\n");
+	else {
+		int value;
+		printf ("Enter a number to delete it:\n");
+		scanf ("%d",&value);
+		struct node *last,*q,*p=head;
+		while (p->next!=head) p=p->next;
+		last=p;
+		p=head;
+		if (head->data==value) {
+			if (head->next==head) {
+				printf ("%d deleted\n",head->data);
+				free(head);
+				return NULL;
+			}
+			else {
+				printf ("%d deleted\n",p->data);
+				head=head->next;
+				free(p);
+				last->next=head;
+			}
+		}
+		else {
+			while (p->next!=head && p->data!=value) {
+				q=p;
+				p=p->next;
+			}
+			if (p->next!=head && p->data==value) {
+				printf ("%d deleted\n",p->data);
+				q->next=p->next;
+				free(p);
+			}
+			else if (p->next==head && p->data==value) {
+				printf ("%d deleted\n",p->data);
+				q->next=head;
+				free(p);
+			}
+			else printf ("No element found\n");
+		}
+	}
+	printf ("**DONE**\n");
+	return head;
+}
+
+void num_of_nodes (struct node *head) {
+	if (head==NULL) printf ("The list is empty so the number of nodes is 0\n");
+	else {
+		struct node *p=head;
+		int c=0;
+		do {
+			c++;
+			p=p->next;
+		}while(p!=head);
+		printf ("The number of nodes is:%d\n",c);
+		printf ("**DONE**\n");
+	}
+}
+
+struct node* destroy (struct node *head) {
+	if (head==NULL) printf ("The list is empty\n");
+	else {
+		struct node *p=head;
+		printf ("The list is destroyed\n");
+		do {
+			head=head->next;
+			free(p);
+			p=head;
+		}while(p!=head);
+		printf ("**DONE**\n");
+	}
+}
