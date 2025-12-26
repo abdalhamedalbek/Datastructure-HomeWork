428369112ffd67ecddae0c86e5a8fc15fa6ed517
abdalhamedalbek <101070815+abdalhamedalbek@users.noreply.github.com>
2025-12-26 20:20:27 +0300

    Graph Clustering (Connected Components).cpp

--- Home work(8).cpp
+++ Home work(8).cpp
@@ -1,9 +1,31 @@
-void dfs(int g[10][10], int v, int visited[]){
-    visited[v]=1;
+#include <stdio.h>
+
+int graph[6][6] = {
+ {0,1,0,0,0,0},
+ {1,0,1,0,0,0},
+ {0,1,0,0,0,0},
+ {0,0,0,0,1,0},
+ {0,0,0,1,0,0},
+ {0,0,0,0,0,0}
+};
+
+int visited[6];
+
+void dfs(int v) {
+    visited[v] = 1;
     printf("%d ", v);
 
-    for(int i=0;i<10;i++)
-        if(g[v][i] && !visited[i])
-            dfs(g,i,visited);
+    for(int i=0;i<6;i++)
+        if(graph[v][i] && !visited[i])
+            dfs(i);
 }
 
+int main() {
+    for(int i=0;i<6;i++) {
+        if(!visited[i]) {
+            printf("Cluster: ");
+            dfs(i);
+            printf("\n");
+        }
+    }
+}
